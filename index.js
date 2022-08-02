dacaea2fc3b43d1347db00624e6f3e34a425c9ea
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2022-08-02 15:26:41 -0300

    feat: add confetti

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ let gapiInited = false
 let gisInited = false
 let responses = [];
 
+/** Components */
 let authorizeButton = document.getElementById('authorize_button');
 let signoutButton = document.getElementById('signout_button');
 let form = document.getElementById('form');
@@ -20,7 +21,8 @@ let formId = document.getElementById('form-id');
 let loadingButton = document.getElementById('loading_button');
 let submitButton = document.getElementById('submit-form');
 let signinButton = document.getElementById('signin_button');
-let modal = document.getElementById('modal');
+let modalWinner = document.getElementById('modal-winner');
+let modalDrums = document.getElementById('modal-drums');
 
 /** Hidde Elements */
 form.classList.add('hidden');
@@ -28,20 +30,6 @@ signoutButton.classList.add('hidden');
 authorizeButton.classList.add('hidden');
 loadingButton.classList.add('hidden');
 signinButton.classList.add('hidden');
-
-/** Confetti */
-var confettiSettings = {
-  target: 'confetti',
-  animate: false,
-  max: 300,
-  props: ['circle', 'square', 'triangle', 'line'],
-  clock: '10',
-  rotate: false,
-
-  // colors: []
-};
-var confetti = new ConfettiGenerator(confettiSettings);
-// confetti.render();
 
 /**
  * Callback after api.js is loaded.
@@ -153,7 +141,7 @@ async function getFormResponsesById(formId) {
   }
 
   responses = response.result.responses
-  modal.classList.remove('hidden');
+  getWinner();
 }
 
 function setLoading(loading) {
@@ -166,6 +154,36 @@ function setLoading(loading) {
     submitButton.classList.remove('hidden');
     // document.getElementById('loading_button').innerText = 'Procurar'
   }
+}
+
+/**
+ * Get winner
+ */
+function getWinner() {
+
+  /** Show modal Drums */
+  modalDrums.classList.remove('hidden');
+  modalWinner.classList.add('hidden');
+
+  setTimeout(() => {
+    /** Hide/Show Modals */
+    modalDrums.classList.add('hidden');
+    modalWinner.classList.remove('hidden');
+    /** Show Cofetti */
+    showConfetti();
+  }, 5000); // 5 seconds
+
+  var item = responses[Math.floor(Math.random() * responses.length)];
+  let name = 'Parabéns Lucas Cardoso!';
+  let email = '';
+
+  console.log(item.answers)
+  for (const [_, value] of Object.entries(item.answers)) {
+    console.log(value.textAnswers.answers[0].value)
+  }
+
+  document.getElementById('winner-name').innerText = name;
+
 }
 
 /**
@@ -188,179 +206,5 @@ function addNotification(message, background) {
 
 /** Close Modal */
 function handleCloseModal() {
-  modal.classList.add('hidden');
-}
-
-responses = [
-  {
-    "responseId": "ACYDBNhxm2qXHc8i4gHQuAc-KWGF-GbOB2KZxfliLi66-dbzICd8TdSfviPcSnkyZE1-2vw",
-    "createTime": "2022-08-01T20:23:28.870Z",
-    "lastSubmittedTime": "2022-08-01T20:23:28.870191Z",
-    "answers": {
-      "1fbbe6ef": {
-        "questionId": "1fbbe6ef",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "jonatas@gmail.com"
-            }
-          ]
-        }
-      },
-      "7835c942": {
-        "questionId": "7835c942",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "Jonatas Maia"
-            }
-          ]
-        }
-      }
-    }
-  },
-  {
-    "responseId": "ACYDBNjr-lJ5s_nBj3uCVGxj4AmjfryGdPKE4Buwd4mSTsBlF3sHEisXyWS0t0oysKVNLxU",
-    "createTime": "2022-08-01T20:23:41.384Z",
-    "lastSubmittedTime": "2022-08-01T20:23:41.384703Z",
-    "answers": {
-      "1fbbe6ef": {
-        "questionId": "1fbbe6ef",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "natalia@gmail.com"
-            }
-          ]
-        }
-      },
-      "7835c942": {
-        "questionId": "7835c942",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "Natalia Santos"
-            }
-          ]
-        }
-      }
-    }
-  },
-  {
-    "responseId": "ACYDBNjz_2wxbrsbHziAsx_DEVncYO6j7zDzoVB8K1Pf9LVgX7-HeiMzCVy1p_ddI02Tv3o",
-    "createTime": "2022-08-01T20:22:42.498Z",
-    "lastSubmittedTime": "2022-08-01T20:22:42.498351Z",
-    "answers": {
-      "1fbbe6ef": {
-        "questionId": "1fbbe6ef",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "luiza@gmail.com"
-            }
-          ]
-        }
-      },
-      "7835c942": {
-        "questionId": "7835c942",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "Luiza Santos"
-            }
-          ]
-        }
-      }
-    }
-  },
-  {
-    "responseId": "ACYDBNip7TZP6oJ0y6ZH06i7nRcEfnoU7BlOeiOF4DMJSAoKCZU8xzYbG3wDrLyLY1VmU9A",
-    "createTime": "2022-08-01T20:21:52.249Z",
-    "lastSubmittedTime": "2022-08-01T20:21:52.249663Z",
-    "answers": {
-      "1fbbe6ef": {
-        "questionId": "1fbbe6ef",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "rodrigo@gmail.com"
-            }
-          ]
-        }
-      },
-      "7835c942": {
-        "questionId": "7835c942",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "Rodrigo Muniz"
-            }
-          ]
-        }
-      }
-    }
-  },
-  {
-    "responseId": "ACYDBNgOItmvHkrclw6hl_Mcf1knh-aDiy1j4kvkJVhCllzUopK4a0nZvMFq0Gz211KaTlI",
-    "createTime": "2022-08-01T20:22:22.476Z",
-    "lastSubmittedTime": "2022-08-01T20:22:22.476239Z",
-    "answers": {
-      "1fbbe6ef": {
-        "questionId": "1fbbe6ef",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "junior@gmail.com"
-            }
-          ]
-        }
-      },
-      "7835c942": {
-        "questionId": "7835c942",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "Junior Santos"
-            }
-          ]
-        }
-      }
-    }
-  },
-  {
-    "responseId": "ACYDBNhZD86GsrpHsGGGllye7Ue1IlQ8SXi9zZgtLK5ojLwvRQkXn4WOCV2w5SGN470GUew",
-    "createTime": "2022-08-01T20:22:03.242Z",
-    "lastSubmittedTime": "2022-08-01T20:22:03.242319Z",
-    "answers": {
-      "1fbbe6ef": {
-        "questionId": "1fbbe6ef",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "thiago@gmail.com"
-            }
-          ]
-        }
-      },
-      "7835c942": {
-        "questionId": "7835c942",
-        "textAnswers": {
-          "answers": [
-            {
-              "value": "Thiago Sampaio"
-            }
-          ]
-        }
-      }
-    }
-  }
-]
-
-var item = responses[Math.floor(Math.random() * responses.length)];
-let name = '';
-let email = '';
-
-console.log(item.answers)
-for (const [_, value] of Object.entries(item.answers)) {
-  console.log(value.textAnswers.answers[0].value)
+  modalWinner.classList.add('hidden');
 }
