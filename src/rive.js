fbc40d8b24dbce848f7e754f50c0c44858e5e5a7
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2022-08-02 11:27:37 -0300

    feat: add dark colors

--- src/rive.js
+++ src/rive.js
@@ -1,5 +1,11 @@
 new rive.Rive({
-  src: 'assets/rive/rive_emoji_pack.riv',
-  canvas: document.getElementById('canvas'),
+  src: 'assets/rive/emoji.riv',
+  canvas: document.getElementById('emoji'),
+  autoplay: true,
+})
+
+new rive.Rive({
+  src: 'assets/rive/roll_the_drums.riv',
+  canvas: document.getElementById('drums'),
   autoplay: true,
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-// const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,7 +11,22 @@ module.exports = {
       },
       backgroundImage: {
         'bg-confetti': "url('../src/assets/svg/confetti.svg')"
-      }
+      },
+      colors: {
+        primary: colors.violet,
+        dark: {
+          50: '#e4e4eb',
+          100: '#bbbace',
+          200: '#8f8ead',
+          300: '#66658c',
+          400: '#4b4777',
+          500: '#302a62',
+          600: '#2b245b',
+          700: '#241c51',
+          800: '#1c1445',
+          900: '#130030',
+        },
+      },
     },
   },
   variants: {
